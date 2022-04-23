eaf30d2412a36a4d5a76679092d97dd8e11b4116
Tawan-Silva <63369670+Tawan-Silva@users.noreply.github.com>
2022-04-22 23:11:16 -0300

    :rocket: Add - Verificação por Recapture no login

--- src/validacaoForm.js
+++ src/validacaoForm.js
@@ -1,12 +1,15 @@
 const form = document.getElementById('formValid');
 const offcanvasBackdrop = document.querySelector('.offcanvas-backdrop');
+const inputNumber = document.getElementById('inputNumber');
+const recaptureCalc = document.querySelector('.recapture');
+
 
 const userLogin = [
     {
         id: 1001,
         username: 'tawansilva',
-        senha: 'cebola123',
-        email: 'tawan.tlsvem@gmail.com'
+        senha: 'cebola',
+        email: 'tawantls@gmail.com'
     },
     {
         id: 1002,
@@ -17,18 +20,19 @@ const userLogin = [
     {
         id: 1003,
         username: 'catiacamara',
-        senha: 'televisao555',
+        senha: 'televisao',
         email: 'televisao@gmail.com'
     }
 ]
+let numRandomOne = Math.floor(Math.random() * 99) + 1;
+let numRandomTwo = Math.floor(Math.floor(Math.random() * 9) + 1)
 
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    const inputEmail = document.getElementById('inputEmail').value;
-    const inputPwd = document.getElementById('inputPwd').value;
-    checkEmail(inputEmail, inputPwd);
-});
+recaptureCalc.innerHTML += `${numRandomOne} + `;
+recaptureCalc.innerHTML += `${numRandomTwo}`;
 
+let result = numRandomOne + numRandomTwo;
+console.log(result);
+// Genjutsu ArrayFormat
 let arrayNames = [];
 let arrayId = [];
 let arrayEmail = [];
@@ -46,49 +50,63 @@ console.log(arrayId);
 console.log(arrayNames);
 
 
-function take(inputEmail, inputPwd) {
-    const indexEmail = arrayEmail.indexOf(inputEmail);
-    console.log(`A index do email é ${indexEmail}`);
-    checkPwd(inputPwd, indexEmail);
-}
 
+// Captura o evento de enviar formulário e (chama a função checkEmail)
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    const inputEmail = document.getElementById('inputEmail').value;
+    const inputPwd = document.getElementById('inputPwd').value;
+    checkEmail(inputEmail, inputPwd);
+});
+
+// Verifica email do usuário é válido e existe na base de dados, e chama função que (coleta a indexEmail)
 function checkEmail(inputEmail, inputPwd) {
     if (/.com$/.test(inputEmail) === false || inputEmail == '' || !arrayEmail.includes(inputEmail)) {
-        return sweetAlertEmailIncorrect()
+        return sweetAlertEmailIncorrect();
     } else {
         console.log(`O email digitado tem no array ? ${arrayEmail.includes(inputEmail)}`);
         take(inputEmail, inputPwd);
     }
 }
 
+// Função que coleta a index do email, e (chama a função checkPwd com a indexEmail e o value da senha do usuário)
+function take(inputEmail, inputPwd) {
+    const indexEmail = arrayEmail.indexOf(inputEmail);
+    console.log(`A index do email é ${indexEmail}`);
+    checkPwd(inputPwd, indexEmail);
+}
+
+// Verifica se a senha do usuário e recebe (index do email válidado)
 function checkPwd(inputPwd, indexEmail) {
     if (inputPwd == '' || !arraySenha.includes(inputPwd)) {
-        return sweetAlertPwdIncorrect()
+        return sweetAlertPwdIncorrect();
     }
     else if (arraySenha.indexOf(inputPwd) !== indexEmail) {
         console.log(`index do inputPwd ${arraySenha.indexOf(inputPwd)}`);
-        return sweetAlertPwdIncorrect()
-    } else {
+        return sweetAlertPwdIncorrect();
+    } 
+    else if(inputNumber.value == '' || inputNumber.value != result) {
+        console.log(inputNumber);
+        return sweetAlertCalcIncorrect();
+    }
+    else {
         storageLocal(arrayId[indexEmail], arrayNames[indexEmail], arrayEmail[indexEmail], arraySenha[indexEmail]);
-        window.location = 'GoodPay/'                                                                
-        // window.location = 'GoodPay/'
+        window.location.reload();                          
     }
 }
 
+// Insere os dados do usuário no LocalStorage após validar o login
 function storageLocal(arrayId, arrayNames, arrayEmail, arraySenha) {
     localStorage.setItem('user', JSON.stringify({ id: arrayId, name: arrayNames, email: arrayEmail, password: arraySenha }));
 }
 
+// Verifica que o usuário está no LocalStorage
 function getStorageLocal() {
-    const userLocal = JSON.parse(localStorage.getItem('user'));
+    // const userLocal = JSON.parse(localStorage.getItem('user'));
     localStorage.getItem('user') !== null ? sweetAlertAllowed() : blockUser();
 }
 
-window.onload = function requireLogin() {
-    getStorageLocal();
-}
-
-
+// Função que abre o NavBar de login e chama função de acesso negado.
 function blockUser() {
     sweetAlertDenied();
 
@@ -119,10 +137,29 @@ function blockUser() {
         accordionCollaspsed.classList.add('show');
         flushCollapseOne.style.border = '1px solid #ff1b3e';
     }, 1000)
-
-
 }
 
+// Alert de acesso permitido.
+function sweetAlertAllowed() {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+      })
+      
+      Toast.fire({
+        icon: 'success',
+        title: 'Acesso permitido'
+      })
+}
+
+// Alert de email e senha incorretos ou inválidos
 function sweetAlertEmailIncorrect() {
     Swal.fire({
         icon: 'error',
@@ -147,29 +184,12 @@ function sweetAlertPwdIncorrect() {
       });
 }
 
-function sweetAlertAllowed() {
-    let timerInterval
-Swal.fire({
-  title: 'Acesso permitido!',
-  html: 'Fechando em <b></b> milissegundos.',
-  timer: 1500,
-  timerProgressBar: true,
-  didOpen: () => {
-    Swal.showLoading()
-    const b = Swal.getHtmlContainer().querySelector('b')
-    timerInterval = setInterval(() => {
-      b.textContent = Swal.getTimerLeft()
-    }, 100)
-  },
-  willClose: () => {
-    clearInterval(timerInterval)
-  }
-}).then((result) => {
-  /* Read more about handling dismissals below */
-  if (result.dismiss === Swal.DismissReason.timer) {
-    console.log('I was closed by the timer')
-  }
-})
+function sweetAlertCalcIncorrect() {
+    Swal.fire({
+        icon: 'error',
+        title: 'Cálculo incorreto ou vázio!',
+        text: `${numRandomOne} + ${numRandomTwo} é igual a: ${result}! Verifique e tente novamente`,
+      });
 }
 
 function sweetAlertDenied() {
@@ -179,4 +199,11 @@ function sweetAlertDenied() {
         text: 'Para continuar entre com sua conta, ou se cadastre!',
         footer: `<a href="../pages/registro.html">Cadastrar</a>`
       }) 
+}
+
+
+
+// função que faz refresh na página
+window.onload = function requireLogin() {
+    getStorageLocal();
 }
